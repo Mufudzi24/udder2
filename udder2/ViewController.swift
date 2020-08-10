7ba40cc9c2dc0c0f290d714117c91408012b280c
Mufudzi Chogugudza <mufudzichojr@gmail.com>
2020-08-10 18:41:21 -0400

    Initial commit

---
+++ udder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder2
+//
+//  Created by Mufudzi Chogugudza on 8/10/20.
+//  Copyright © 2020 Mufudzi Chogugudza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
